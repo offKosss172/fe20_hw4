c687f49240db6b11c398abb2223f841f962fddad
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-08-25 00:24:05 +0300

    main commit

--- sript.js
+++ sript.js
@@ -1,26 +1,53 @@
 alert('hello')
 
-//первое задание
-const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
+let allCharacters = []; // Объявляем переменную allCharacters перед обоими fetch запросами
+
+function showMeStarWars() {
   fetch("https://ajax.test-danit.com/api/swapi/films", requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => {
+      const showCard = document.querySelector(".showCard");
+
+      result.forEach((film, index) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const episodeId = document.createElement("h2");
+        episodeId.classList.add("desc-episodeId");
+        episodeId.innerText = `Episode ${film.episodeId}`;
+
+        const name = document.createElement("h2");
+        name.classList.add("desc-name");
+        name.innerText = film.name;
+
+        const openingCrawl = document.createElement("p");
+        openingCrawl.classList.add("desc-opening");
+        openingCrawl.innerText = film.openingCrawl;
+
+        const actors = document.createElement("p");
+        actors.classList.add("desc-actors");
+        actors.innerText = allCharacters[index].characters.join(", "); // Отображаем имена персонажей
+
+        card.appendChild(episodeId);
+        card.appendChild(name);
+        card.appendChild(openingCrawl);
+        card.appendChild(actors);
+
+        showCard.appendChild(card);
+      });
+    })
     .catch(error => console.log('error', error));
+}
 
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
 
-
-//второе задание 
 fetch("https://ajax.test-danit.com/api/swapi/films", requestOptions)
   .then(response => response.json())
   .then(async result => {
-    const allCharacters = [];
-
     for (const film of result) {
-
       const charactersPromises = film.characters.map(characterUrl => 
         fetch(characterUrl, requestOptions)
           .then(response => response.json())
@@ -36,57 +63,7 @@ fetch("https://ajax.test-danit.com/api/swapi/films", requestOptions)
     }
 
     console.log(allCharacters);
+    
+    showMeStarWars(); // Вызываем функцию после загрузки данных
   })
   .catch(error => console.log('Ошибка при получении списка фильмов', error));
-  
-
-
-
-  function showMeStarWars() {
-    alert("готово")
-  }
-  showMeStarWars()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-    class showCard {
-        constructor({episodeId, name, openingCrawl}) {
-            this.episodeId = episodeId,
-            this.name = name,
-            this.openingCrawl = openingCrawl
-        }
-    
-        render() {
-            const card = document.createElement('div');
-            card.classList.add('card');
-            card.dataset.episodeId = this.episodeId;
-    
-            
-            card.innerHTML = `
-
-            <div class="card-body">
-                <h6 class="title">номер епізоду:</h6>
-                <p>${this.visitor}</p>
-                <h6 class="title">назва:</h6>
-                <p>${this.doctor}</p>
-                <h6 class="title">опис епізода:</h6>
-                <p>${this.doctor}</p>
-            </div>
-            `;
-            document.querySelector('.visits').prepend(card);
-        }
-    } 
